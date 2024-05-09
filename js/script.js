ff91064f14a13a97c608802e356c2ed9ab40b02e
raghveerdeol <deolraghveer5@gmail.com>
2024-05-09 23:09:07 +0200

    added querySelector and adjusted if

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
 
 
+
+
+const bottone = document.querySelector('button');
+
+
+bottone.addEventListener('click', function () {
 // Il numero di chilometri da percorrere 
-const userDistance = prompt('Inserire la distanza da percorere');
+let userDistance  = document.querySelector('div > input#userDistance');
 // Età del passeggero 
-const userAge = prompt('Inserire l\'età del passeggero');
+let userAge = document.querySelector('div > input#userAge');
 // prezzo del biglietto al km (0.267113 € al km) 
-const kmPrice = userDistance * 0.267113;
+const kmPrice = userDistance.value * 0.267113;
 let sconto = 0;
 let price = 0;
-if (userAge < 18) {
+
+if (userAge.value < 18) {
     // va applicato uno sconto del 24.552% per gli under 21 
     sconto = 24.552;
-} else if (userAge >= 65 ){
+} else if (userAge.value >= 65 ){
     // va applicato uno sconto del 37.893% per gli over 63 
     sconto = 37.893;
 } else {
@@ -19,4 +26,6 @@ if (userAge < 18) {
 }
 price = kmPrice - ((kmPrice / 100) * sconto);
 
-console.log(price);
+
+    console.log(userDistance.value, userAge.value,  kmPrice,  price)
+})
